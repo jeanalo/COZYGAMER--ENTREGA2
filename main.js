5f775f7d7f1bd84f866f91f11960080df0ed6f45
Jean alomia <Jeanalo>
2024-05-08 13:55:08 -0500

    Footer functionality

--- main.js
+++ main.js
@@ -18,19 +18,25 @@ const displayCards = (juegos) => {
 };
 
 const displayForYou = (juegos) => {
+    // Colocar list-item en vez de swiper
     const swiperWrapper = document.querySelector('.swiper-wrapper');
 
+    // copiar el for
     for (let i = 0; i < 3; i++) {
         const rand = Math.floor(Math.random() * juegos.length);
         const selection = juegos[rand];
         const juegoInstance = new Juego(selection.id, selection.descripcion, selection.estudio, selection.titulo, selection.imagen, selection.link);
         const card = juegoInstance.renderGameCard();
 
+        // Ignorar las partes de slide y reemplazar para insertar en el list-items
         const slide = document.createElement('div');
-        slide.classList.add('swiper-slide');
+        slide.classList.add('swsiper-slide');
 
+        // insertar en list-items 
         slide.appendChild(card);
         swiperWrapper.appendChild(slide);
+
+        // se copia esta linea despues del appendChild tal cual
         juegoInstance.addEventListeners();
     }
 }
